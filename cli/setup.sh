9f4da0c16fbc17db61a48f853cd2bb4dac9b1fe4
harinikanakala <162731050+harinikanakala@users.noreply.github.com>
2024-05-08 23:49:29 -0400

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
+GROUP="ml-cranium-poc-rg"
 LOCATION="eastus"
-WORKSPACE="main"
+WORKSPACE="ML_Cranium_WS"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
